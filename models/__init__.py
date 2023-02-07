384597d223417de96571579ab9d7e4ed40d0e8ba
Oluwatobi <amure387@gmail.com>
2023-02-07 21:15:55 +0100

    push

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from models.engine import *
+from models.engine.file_storage import FileStorage
 
-storage = file_storage.FileStorage()
+storage = FileStorage()
 storage.reload()
