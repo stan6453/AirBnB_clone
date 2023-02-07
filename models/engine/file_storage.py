9e93e94eeed36a65e21597bd186978b74d43d85b
Oluwatobi <amure387@gmail.com>
2023-02-08 00:23:10 +0100

    edited file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,17 +11,20 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        self.__objects[f'{obj.__class.__name__}.{obj.id}'] = obj
+        self.__objects[f'{obj.__class__.__name__}.{obj.id}'] = obj
         
     def save(self):
-        obj_dict = {k: v.to_dict() for k, v in self.__objects.items()}
+        obj_dict = {k: v.to_dict() for k, v in self.__objects.items() if hasattr(v, 'to_dict')}
         with open(self.__file_path, 'w') as file:
             json.dump(obj_dict, file)
 
     def reload(self):
         try:
             with open(self.__file_path, 'r') as file:
-                self.__objects = json.load(file)
+                obj_dict = json.load(file)
+                for k, v in obj_dict.items():
+                    obj_dict[k] = eval(f'{v.__class__.__name__}(**v)')
+					self.__objects = obj_dict
         except FileNotFoundError:
             pass
 
