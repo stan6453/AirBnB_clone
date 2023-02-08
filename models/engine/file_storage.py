b7d42e3cce7fc724dadfe3f83c761d00cb5b0285
Oluwatobi <amure387@gmail.com>
2023-02-08 14:32:34 +0100

    create all classes that subclass BaseModel

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+'''
+Module for FileNotFoundError Class
+'''
 import json
 
 class FileStorage:
@@ -8,17 +10,29 @@ class FileStorage:
     __objects = {}
 
     def all(self):
+        '''Returns the dictionary __objects'''
         return self.__objects
 
     def new(self, obj):
+		'''
+        Sets in __objects the obj with key <obj class name>.id
+        Args: obj
+        '''
         self.__objects[f'{obj.__class__.__name__}.{obj.id}'] = obj
         
     def save(self):
+		'''
+        Serializes __objects to the JSON file (path: __file_path)
+        '''
         obj_dict = {k: v.to_dict() for k, v in self.__objects.items() if hasattr(v, 'to_dict')}
         with open(self.__file_path, 'w') as file:
             json.dump(obj_dict, file)
 
     def reload(self):
+		'''
+        Deserializes the JSON file to __objects (only if the JSON file\
+        (__file_path) exists ; otherwise, do nothing.
+        '''
         try:
             with open(self.__file_path, 'r') as file:
                 obj_load = json.load(file)
