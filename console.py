3186d36e964177a570f73cd41cde0ba305d07e8b
stanley <stanley6453@gmail.com>
2023-02-10 17:51:14 +0100

    updated console.py

--- console.py
+++ console.py
@@ -9,6 +9,11 @@ import shlex
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.state import State
+from models.review import Review
 
 CLASSES = [
     "BaseModel",
