a1f17c2f2add42a157aa58805145c021ce6e29b8
Oluwatobi <amure387@gmail.com>
2023-02-10 09:49:04 +0100

    pycodestyle

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+Module For Amenity Class
+'''
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''Amenity Class that inherits from BaseModel'''
+    name = ''
