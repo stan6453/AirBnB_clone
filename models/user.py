65becc73e94c3d6fc581eb64925ac73c7e9cfd76
Oluwatobi <amure387@gmail.com>
2023-02-11 14:33:52 +0100

    fixed user file

--- models/user.py
+++ models/user.py
@@ -5,13 +5,10 @@ Module For User Class
 import models
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
     '''User Class that inherits from BaseModel'''
     email = ''
     password = ''
     first_name = ''
     last_name = ''
-
-    @classmethod
-    def all(cls):
-        return [cls(**obj.__dict__) for obj in models.storage.all().values() if obj.__class__.__name__ == cls.__name__] 
