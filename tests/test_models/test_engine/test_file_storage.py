33b8e34898e409dd5e489c0e5c2a1f4818cc3a55
Oluwatobi <amure387@gmail.com>
2023-02-12 15:26:58 +0100

    push

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+'''
+Unittests for FileStorage Class
+'''
+import unittest
+import json
+from models import base_model, storage
+from models.engine.file_storage import FileStorage
+import os
+
+
+class TestFileStorage(unittest.TestCase):
+    '''Class to run unittests for FileStorage'''
+    def setup(self):
+        '''Setup instance of BaseModel to run tests'''
+        base = base_model.BaseModel()
+    def delete_file(self):
+        '''Delete file if it exists'''
+        storage._FileStorage__objects = {}
+        if os.path.exists('file.json'):
+             os.remove('file.json')
+    def test_type(self):
+        self.assertEqual(type(storage), FileStorage)
+        self.assertEqual(type(storage._FileStorage__file_path), str)
+        self.assertEqual(type(storage._FileStorage__objects), dict)
+        self.assertEqual(type(storage.all()), dict)
+    def test_all(self):
+        self.delete_file()
+        self.assertEqual(storage.all(), {})
+        with self.assertRaises(TypeError):
+            storage.all(1)
+            storage.all('string')
+            storage.all(None)
+    def test_new(self):
+        self.delete_file()
+        base = base_model.BaseModel()
+        storage.new(base)
+        self.assertEqual(storage.all(), {f'{base.__class__.__name__}.{base.id}': base}) 
+        self.assertEqual(len(storage.all()), 1) 
+        with self.assertRaises(TypeError):
+            storage.new(base, 1)
+            storage.new('test.json')
+        with self.assertRaises(AttributeError):
+            storage.new(None)
+    def test_save(self):
+        self.delete_file()
+        base = base_model.BaseModel()
+        storage.new(base)
+        storage.save()
+        with open('file.json', 'r') as file:
+            self.assertEqual(json.load(file), {f'{base.__class__.__name__}.{base.id}': base.to_dict()})
+        with self.assertRaises(TypeError):
+            storage.save(1)
+            storage.save('string')
+            storage.save(None)
+    def test_reload(self):
+        self.delete_file()
+        base = base_model.BaseModel()
+        storage.new(base)
+        base_id = f'{base.__class__.__name__}.{base.id}'
+        storage.save()
+        storage.reload()
+        self.assertEqual(storage.all()[base_id].to_dict(), base.to_dict())
+        with self.assertRaises(TypeError):
+            storage.reload(1)
+            storage.reload('string')
+            storage.reload(None)
+if __name__ == '__main__':
+    unittest.main()
