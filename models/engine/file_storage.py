278618ec57dbe59e7c0786dd557ba7c1f745edfc
stanley <stanley6453@gmail.com>
2023-02-13 19:44:52 +0100

    modified file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,20 +20,20 @@ class FileStorage:
 
     def all(self):
         '''Returns the dictionary __objects'''
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         '''
         Sets in __objects the obj with key <obj class name>.id
         Args: obj
         '''
-        self.__objects[f'{obj.__class__.__name__}.{obj.id}'] = obj
+        FileStorage.__objects[f'{obj.__class__.__name__}.{obj.id}'] = obj
 
     def save(self):
         '''
         Serializes __objects to the JSON file (path: __file_path)
         '''
-        obj_dict = {k: v.to_dict() for k, v in self.__objects.items()}
+        obj_dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
         with open(self.__file_path, 'w') as file:
             json.dump(obj_dict, file)
 
@@ -43,9 +43,9 @@ class FileStorage:
         (__file_path) exists ; otherwise, do nothing.
         '''
         try:
-            with open(self.__file_path, 'r') as file:
+            with open(FileStorage.__file_path, 'r') as file:
                 obj_load = json.load(file)
                 for k, v in obj_load.items():
-                    self.__objects[k] = eval(v["__class__"])(**v)
+                    FileStorage.__objects[k] = eval(v["__class__"])(**v)
         except FileNotFoundError:
             pass
